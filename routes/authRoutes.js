b3c2d785ee580022c12a32b04e80ea5a4ba612f9
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-13 13:12:05 -0700

    Changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const User = require('../model/user');
 const router  = express.Router();
 
